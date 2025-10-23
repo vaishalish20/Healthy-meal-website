f75b46213883b780b5f1ea413b92b7b128e195b6
Nitesh-loop <nitesh166k@gmail.com>
2025-10-23 14:52:23 +0530

    Js challenge 1

---
+++ js-c1/index.js
@@ -0,0 +1,40 @@
+//challenge 1
+// let firstname = "Nick"
+// let lastname = "Saga"
+
+// let fullname = firstname + " " + lastname
+
+// console.log(fullname)
+
+// -----------------------------------------------------------------------
+
+//challenge 2
+// let name = "Linda"
+// let greeting = "Hi there"
+
+// function welcomeCall() {
+//     console.log(greeting + ", " + name + "!")
+// }
+
+// welcomeCall()
+
+// -----------------------------------------------------------------------
+
+//challenge 3
+
+let myPoints = 3
+function add3Points() {
+    myPoints += 3
+}
+
+function remove1Points() {
+    myPoints -= 1
+}
+
+add3Points()
+add3Points()
+add3Points()
+remove1Points()
+remove1Points()
+
+console.log(myPoints)
